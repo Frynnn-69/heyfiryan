ca62e3fc1f93acece724a0c15abeb84dc3c74766
Firyan <firyanfatihf01@gmail.com>
2026-01-17 05:50:27 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import path from "node:path"; 
+import { fileURLToPath } from "node:url"; 
 
-// https://astro.build/config
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   site: 'https://heyfiryan.id',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   },
 });
